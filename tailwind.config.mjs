879405b1058fe1ddd6d663a45f6ee8a8fb10bfe9
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2024-04-16 17:11:10 -0600

    Improve gallery (No se decirlo en español curiosamente)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,14 @@
-import animations from '@midudev/tailwind-animations'
+import animations from "@midudev/tailwind-animations";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [animations],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      aspectRatio: {
+        "3/4": "3/4",
+      },
+    },
+  },
+  plugins: [animations],
+};
